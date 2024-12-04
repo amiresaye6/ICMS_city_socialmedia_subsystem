00948619ae5d6446ff1859606c5ccf0ca0c476fd
Amir Alsayed <amyralsyd367@gmail.com>
2024-12-04 20:19:53 +0200

    adding posts routes and cotrollers

--- Controllers/posts.controller.js
+++ Controllers/posts.controller.js
@@ -1,10 +1,52 @@
-const Posts = require("../Models/posts.model");
+const { validationResult } = require("express-validator");
+const Post = require("../Models/posts.model");
 
 exports.getPosts = (req, res) => {
-    
-    res.status(200).json(
-        {
-            message: "post created succesfully"
-        }
-    )
+    const result = validationResult(req);
+    if (!result.isEmpty()) {
+        return res.status(400).send({ errors: result.array() });
+    }
+    res.status(200).json({
+        message: "Post retrieved successfully"
+    });
+}
+
+exports.createPost = async (req, res) => {
+    try {
+        const newPost = await Post.create(req.body);
+        res.status(201).json(newPost);
+        // res.status(201).json(newPost);
+    } catch (error) {
+        res.status(400).json({ error: error.message });
+    }
+}
+
+exports.addReacts = async (req, res) => {
+    // add Reacts logic for esraa
+}
+
+exports.addToShaere = async (req, res) => {
+    // the logic to add new user to the share list for esraa
+}
+
+exports.delete = async (req, res) => {
+    // delete logic
+    const id = req.params.hamada
+    const result = await Post.deleteOne({_id: id})
+    res.json(result)
+    res.json({ id })
+}
+
+exports.updateCaption = async (req, res) => {
+    try {
+        const id = req.params.id
+        const updatedPost = await Post.findByIdAndUpdate(
+            id, // Document ID
+            { $set: { postCaption: req.body.newCaption } }, // Update operation
+            { new: true } // Return the updated document
+        );
+        res.status(201).json(updatedPost);
+    } catch (error) {
+        console.error(error.message);
+    }
 }
--- Routes/posts.routes.js
+++ Routes/posts.routes.js
@@ -1,6 +1,16 @@
 const router = require('express').Router();
 const postsController = require("../Controllers/posts.controller");
 
-router.get("/", postsController.getPosts)
+router.post("/addPost", postsController.createPost);
+
+// esraa task
+router.put("/add-react", postsController.addReacts);
+router.put("/add-share", postsController.addToShaere);
+
+
+
+router.delete("/:hamada", postsController.delete)
+router.put('/upadeCaptio/:id', postsController.updateCaption)
+
 
 module.exports = router;
