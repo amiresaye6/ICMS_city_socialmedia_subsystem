1ec7b98c5b59050d7375f695520de36cffb96f10
Amir Alsayed <amyralsyd367@gmail.com>
2025-04-11 18:04:33 +0200

    adding change bio endpoint

--- Routes/users.routes.js
+++ Routes/users.routes.js
@@ -9,6 +9,7 @@ router.get('/:userId', centralAuthMiddleware.centralAuthenticate, usersControlle
 router.put('/me', centralAuthMiddleware.centralAuthenticate, usersController.changeUserName);
 router.put('/me/avatar', centralAuthMiddleware.centralAuthenticate, upload.array("avatar", 1), usersController.changeAvatar);
 router.put('/me/cover', centralAuthMiddleware.centralAuthenticate, upload.array("cover", 1), usersController.changeCover);
+router.put('/me/bio', centralAuthMiddleware.centralAuthenticate, usersController.changeBio);
 
 
 module.exports = router;
